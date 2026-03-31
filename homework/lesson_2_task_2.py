584bd4573e0adbee258811ed35f2545d08b4b0af
@_Evgenya_Leonova <Eva.new@inbox.ru>
2026-03-31 07:45:44 +0300

    Delete homework directory

--- homework/lesson_2_task_2.py
+++
@@ -1,3 +0,0 @@
-my_age = 34
-my_age = 38
-print(my_age)
--- homework/lesson_3_task_3.py
+++
@@ -1,4 +0,0 @@
-first_name = input('Как Вас зовут?: ')
-last_name = input('Ваша фамилия?: ')
-
-print(f'Вас зовут: {first_name} {last_name}')
