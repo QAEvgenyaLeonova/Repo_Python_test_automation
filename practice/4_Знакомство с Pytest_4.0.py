e25f477298d10bda2998b762b2b6c0e34ba8fe9e
Evgenya-odyvanchik <Eva.new@inbox.ru>
2026-04-16 15:37:54 +0300

    пр 4/1

--- practice/4_Знакомство с Pytest_4.0.py
+++ practice/4_Знакомство с Pytest_4.0.py
@@ -139,6 +139,46 @@ def test_avg_positive():
     res = calculator.avg(numbers)
     assert res == 5
 
+#файл(упрощение)
+
+import pytest
+from calculator import Calculator
+
+calculator = Calculator()            #Создание экземпляра класса, #  глобальный экземпляр
+
+@pytest.mark.parametrize('num1, num2, result', [(4, 5, 9), (-6, -10, -16), (-6, 6, 0), (5.61, 4.29, 9.9), (10, 0, 10)])
+def test_sum_positive_nums(num1, num2, result):
+    calculator = Calculator()
+    res = calculator.sum(num1, num2)
+    assert res == result
+
+@pytest.mark.positive_test
+def test_div_positive():
+    calculator = Calculator()
+    res = calculator.div(10, 2)
+    assert res == 5
+
+def test_div_by_zero():
+    calculator = Calculator()
+    with pytest.raises(ZeroDivisionError):
+        calculator.div(10, 0)
+
+@pytest.mark.parametrize('nums, expected', [([], 0),([1, 2, 3, 4, 5, 6, 7, 8, 9, 5], 5)])
+def test_avg_list(nums, expected):
+    calculator = Calculator()
+    res = calculator.avg(nums)
+    assert res == expected
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- practice/4_Знакомство с Pytest_4.1.py
+++ practice/4_Знакомство с Pytest_4.1.py
@@ -16,3 +16,31 @@ addopts = -v -s
 python_classes = *Suites *Test
 python_files = check_*
 python_functions = assert_*'''
+
+'''@pytest.mark.skip(reason='починить тест позже')
+def test_sum_positive_nums():        
+    calculator = Calculator()       
+    res = calculator.sum(4, 5) 
+    assert res == 9 
+    
+
+@pytest.mark.xfail - говорит о том что мы специально повредили тест и это не ошибка
++ можно указать @pytest.mark.xfail(strict = True) точно упадет или нет?'''
+
+'''своя маркировка pytest -m positive_test
+@pytest.mark.positive_test
+def test_avg_positive():
+    calculator = Calculator()
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 5]
+    res = calculator.avg(numbers)
+    assert res == 5'''
+
+''' pytest --markers'''
+
+'''parametriz
+
+@pytest.mark.parametriz('num1, num2, result', [(4, 5, 9)])
+def test_sum_positive_nums(num1, num2, result):       
+    calculator = Calculator()                     
+    res = calculator.sum(num1, num2)                
+    assert res == result  '''
