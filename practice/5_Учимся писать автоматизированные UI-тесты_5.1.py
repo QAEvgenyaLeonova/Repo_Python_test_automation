7bfd4302c7c1c17f70ad1396cc5a51efc3969295
Evgenya-odyvanchik <Eva.new@inbox.ru>
2026-04-28 12:27:33 +0300

    Практика Selenium

--- practice/5_Учимся писать автоматизированные UI-тесты_5.1.py
+++ practice/5_Учимся писать автоматизированные UI-тесты_5.1.py
@@ -1,27 +1,16 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+# Вариант 1: Если используете ручной путь к chromedriver.exe
+service = Service('C:\\Users\\user\\Documents\\2.НЕ ПЕРЕКИНУТОЕ\\5.Автоматизация тестирования на Python\\Урок_5.Знакомство с Selenium\\Хром драйвер 2\\chromedriver.exe')
+driver = webdriver.Chrome(service=service)
 
+# ИЛИ (выберите только один вариант — либо выше, либо ниже)
 
+# Вариант 2: Если хотите автоматическую загрузку ChromeDriver (рекомендуется)
+# driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sleep(50)
